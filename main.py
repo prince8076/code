2ae3c0d53378c7d6176aa5bfe50339e5e3f43372
prince8076 <rahulsingh00c@gmail.com>
2025-07-15 00:44:47 +0530

    handle whitespace and empty entries

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ def generate_number(n):
     result = []
     parts = n.split(',')
     for i in parts:
-        i = i.strip()
+        i=i.strip()
+        if not i:
+            continue
+        
         if '-' in i:
             start_str, end_str = i.split('-')
             start, end = int(start_str), int(end_str)
             result.extend(range(start, end + 1))
-        elif i:
+        else:
             result.append(int(i))
     return result
 
