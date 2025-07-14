d302bd081bebc539be7b1729e1aa6cc928e28753
prince8076 <rahulsingh00c@gmail.com>
2025-07-15 00:53:45 +0530

    add custom delimiters like '..','to,'~''

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 def generate_number(n):
     result = []
+    delimiters = ['-','..','to','~']
     parts = n.split(',')
     for i in parts:
         i=i.strip()
         if not i:
             continue
-        
-        if '-' in i:
-            start_str, end_str = i.split('-')
-            start, end = int(start_str), int(end_str)
-            result.extend(range(start, end + 1))
-        else:
+            
+        matched= False
+        for j in delimiters:
+            if j in i:
+                start_str, end_str = i.split(j)
+                start, end = int(start_str), int(end_str)
+                result.extend(range(start, end + 1))
+                matched=True
+                break
+
+        if not matched:
             result.append(int(i))
     return result
 
